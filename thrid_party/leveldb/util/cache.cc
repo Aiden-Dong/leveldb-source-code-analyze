8408e559268bf0a601f0f5baacd9b6c5132bd0ff
aiden.dong <782112163@qq.com>
2022-03-01 18:58:38 +0800

    add

--- thrid_party/leveldb/util/cache.cc
+++ thrid_party/leveldb/util/cache.cc
@@ -21,24 +21,6 @@ namespace {
 
 // LRU cache implementation
 // LRU 策略 : 淘汰最近最久未使用的缓存
-//
-// Cache entries have an "in_cache" boolean indicating whether the cache has a
-// reference on the entry.  The only ways that this can become false without the
-// entry being passed to its "deleter" are via Erase(), via Insert() when
-// an element with a duplicate key is inserted, or on destruction of the cache.
-//
-// The cache keeps two linked lists of items in the cache.  All items in the
-// cache are in one list or the other, and never both.  Items still referenced
-// by clients but erased from the cache are in neither list.  The lists are:
-// - in-use:  contains the items currently referenced by clients, in no
-//   particular order.  (This list is used for invariant checking.  If we
-//   removed the check, elements that would otherwise be on this list could be
-//   left as disconnected singleton lists.)
-// - LRU:  contains the items not currently referenced by clients, in LRU order
-// Elements are moved between these lists by the Ref() and Unref() methods,
-// when they detect an element in the cache acquiring or losing its only
-// external reference.
-
 
 /****
  * 即用于 hashtable , 又用于lru缓存节点
