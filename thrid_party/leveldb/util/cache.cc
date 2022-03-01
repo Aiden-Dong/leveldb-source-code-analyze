97a2f1ec748f7e309e00d39298463fd777a71a10
aiden.dong <782112163@qq.com>
2022-03-01 16:00:07 +0800

    add

--- thrid_party/leveldb/util/cache.cc
+++ thrid_party/leveldb/util/cache.cc
@@ -20,6 +20,7 @@ Cache::~Cache() {}
 namespace {
 
 // LRU cache implementation
+// LRU 策略 : 淘汰最近最久未使用的缓存
 //
 // Cache entries have an "in_cache" boolean indicating whether the cache has a
 // reference on the entry.  The only ways that this can become false without the
@@ -38,80 +39,93 @@ namespace {
 // when they detect an element in the cache acquiring or losing its only
 // external reference.
 
-// An entry is a variable length heap-allocated structure.  Entries
-// are kept in a circular doubly linked list ordered by access time.
-struct LRUHandle {
-  void* value;
-  void (*deleter)(const Slice&, void* value);
-  LRUHandle* next_hash;
-  LRUHandle* next;
-  LRUHandle* prev;
-  size_t charge;  // TODO(opt): Only allow uint32_t?
-  size_t key_length;
-  bool in_cache;     // Whether entry is in the cache.
-  uint32_t refs;     // References, including cache reference, if present.
-  uint32_t hash;     // Hash of key(); used for fast sharding and comparisons
-  char key_data[1];  // Beginning of key
 
+/****
+ * 即用于 hashtable , 又用于lru缓存节点
+ */
+struct LRUHandle {
+  void* value;                                          // 具体的值， 指针类型
+  void (*deleter)(const Slice&, void* value);           // 定义回收节点的回调函数
+  LRUHandle* next_hash;                                 // 使用在hashtable中，挂载同一个hash值上的单链表结构
+  LRUHandle* next;                                      // 代表 LRU 双向链表中的下一个节点
+  LRUHandle* prev;                                      // 代表 LRU 双向链表中的上一个节点
+  size_t charge;                                        // 记录当前 value 所占用的内存大小， 用于后面超出容量后需要进行lru
+  size_t key_length;                                    // 数据 key 的长度
+  bool in_cache;                                        // 表示是否在缓存中
+  uint32_t refs;                                        // 引用计数，因为当前节点可能会被多个组件使用， 不能简单的删除
+  uint32_t hash;                                        // 记录当前key 的hasn值
+  char key_data[1];                                     // key 数据位置
+
+  // 获取 key
   Slice key() const {
-    // next is only equal to this if the LRU handle is the list head of an
-    // empty list. List heads never have meaningful keys.
     assert(next != this);
 
     return Slice(key_data, key_length);
   }
 };
 
-// We provide our own simple hash table since it removes a whole bunch
-// of porting hacks and is also faster than some of the built-in hash
-// table implementations in some of the compiler/runtime combinations
-// we have tested.  E.g., readrandom speeds up by ~5% over the g++
-// 4.4.3's builtin hashtable.
+/***
+ * HashTable : 数组加单链表结构
+ *      1. hash table 的容量大小默认为0，
+ *      2. 而且如果元素少于4时，hash table 按照元素的个数来分配
+ *      3. 如果元素大于4时， 则按照2的倍数对齐
+ *      4. 对于冲突采用链地址法
+ *      5. rehash 的实际是当元素的个数大于链表长度时
+ */
 class HandleTable {
  public:
   HandleTable() : length_(0), elems_(0), list_(nullptr) { Resize(); }
   ~HandleTable() { delete[] list_; }
 
+  // 查找指定key的数据节点
   LRUHandle* Lookup(const Slice& key, uint32_t hash) {
     return *FindPointer(key, hash);
   }
 
+
+  /***
+   * 插入一个新的节点元素， 如果key存在就替换
+   */
   LRUHandle* Insert(LRUHandle* h) {
-    LRUHandle** ptr = FindPointer(h->key(), h->hash);
+    LRUHandle** ptr = FindPointer(h->key(), h->hash);  // 查找 hashtable 中是否存在这个节点
     LRUHandle* old = *ptr;
     h->next_hash = (old == nullptr ? nullptr : old->next_hash);
-    *ptr = h;
-    if (old == nullptr) {
+    *ptr = h;  // 如果不存在， 进行尾部插入
+
+    if (old == nullptr) { // 表示插入操作
       ++elems_;
-      if (elems_ > length_) {
-        // Since each cache entry is fairly large, we aim for a small
-        // average linked list length (<= 1).
+      if (elems_ > length_) { // 数据节点超过
         Resize();
       }
     }
     return old;
   }
 
+  /***
+   * 移出对应key的hash节点, 返回给用户， 不做清理操作
+   */
   LRUHandle* Remove(const Slice& key, uint32_t hash) {
     LRUHandle** ptr = FindPointer(key, hash);
     LRUHandle* result = *ptr;
     if (result != nullptr) {
-      *ptr = result->next_hash;
+      *ptr = result->next_hash; // 摘掉此节点
       --elems_;
     }
     return result;
   }
 
  private:
-  // The table consists of an array of buckets where each bucket is
-  // a linked list of cache entries that hash into the bucket.
-  uint32_t length_;
-  uint32_t elems_;
-  LRUHandle** list_;
+  uint32_t length_;    // 当前 hash 表的总长度
+  uint32_t elems_;     // 当前 hash 表实际元素个数
+  LRUHandle** list_;  //  实际存储数据，底层结构， 每一个元素是一个指向 LRUHandler 的指针
 
-  // Return a pointer to slot that points to a cache entry that
-  // matches key/hash.  If there is no such cache entry, return a
-  // pointer to the trailing slot in the corresponding linked list.
+
+  /***
+   * 在 hash 表中查找元素
+   * @param key 用于在同一个hash链上的key比对
+   * @param hash 定位hash点
+   * @return 指向对应 key 元素的 LRUHandler 指针， 使用二维指针的原因，可能要进行修改操作
+   */
   LRUHandle** FindPointer(const Slice& key, uint32_t hash) {
     LRUHandle** ptr = &list_[hash & (length_ - 1)];
     while (*ptr != nullptr && ((*ptr)->hash != hash || key != (*ptr)->key())) {
@@ -120,26 +134,33 @@ class HandleTable {
     return ptr;
   }
 
+  // 链表扩容
   void Resize() {
-    uint32_t new_length = 4;
-    while (new_length < elems_) {
+    uint32_t new_length = 4;  // 默认 4 个
+    while (new_length < elems_) { // 如果元素超过 4个， 则向上 *2 对齐
       new_length *= 2;
     }
-    LRUHandle** new_list = new LRUHandle*[new_length];
+    LRUHandle** new_list = new LRUHandle*[new_length]; // 二维指针
+
+    // 初始化 new_list -> nullptr
     memset(new_list, 0, sizeof(new_list[0]) * new_length);
     uint32_t count = 0;
     for (uint32_t i = 0; i < length_; i++) {
+      // 遍历当前的 list_
       LRUHandle* h = list_[i];
       while (h != nullptr) {
-        LRUHandle* next = h->next_hash;
+        LRUHandle* next = h->next_hash;  // 获取当前节点指向的下一个节点
+        // 重新计算当前节点应该挂载哪一个链上
         uint32_t hash = h->hash;
         LRUHandle** ptr = &new_list[hash & (new_length - 1)];
+        // 放置在链的首部
         h->next_hash = *ptr;
         *ptr = h;
         h = next;
         count++;
       }
     }
+    // 清理旧的hash数组
     assert(elems_ == count);
     delete[] list_;
     list_ = new_list;
