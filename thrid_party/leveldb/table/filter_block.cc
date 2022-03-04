adce1af92dc5ec63106e7c9fba62a8b370d88ccb
aiden.dong <782112163@qq.com>
2022-03-04 19:41:02 +0800

    add

--- thrid_party/leveldb/table/filter_block.cc
+++ thrid_party/leveldb/table/filter_block.cc
@@ -13,7 +13,7 @@ namespace leveldb {
 
 // Generate new filter every 2KB of data
 static const size_t kFilterBaseLg = 11;                       //
-static const size_t kFilterBase = 1 << kFilterBaseLg;       /// 2KB 数据构建一个 BloomFilter
+static const size_t kFilterBase = 1 << kFilterBaseLg;         // 2KB 数据构建一个 BloomFilter
 
 FilterBlockBuilder::FilterBlockBuilder(const FilterPolicy* policy)
     : policy_(policy) {}
